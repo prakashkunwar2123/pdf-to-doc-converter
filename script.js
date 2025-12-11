66772e5707a8c84b86500888075429c9d870bde7
prakashkunwar2123 <prakashkunwar2123@gmail.com>
2025-12-11 16:35:56 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+async function convertPDF() {
+  let file = document.getElementById("pdfFile").files[0];
+  if (!file) {
+    alert("Please upload a PDF");
+    return;
+  }
+
+  document.getElementById("status").innerText = "Converting...";
+
+  let fileReader = new FileReader();
+  fileReader.onload = async function () {
+    let typedArray = new Uint8Array(this.result);
+    let pdf = await pdfjsLib.getDocument(typedArray).promise;
+
+    let textContent = "";
+
+    for (let i = 1; i <= pdf.numPages; i++) {
+      let page = await pdf.getPage(i);
+      let text = await page.getTextContent();
+      text.items.forEach(item => textContent += item.str + " ");
+    }
+
+    let blob = new Blob([textContent], { type: "application/msword" });
+    let link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "converted.doc";
+    link.click();
+
+    document.getElementById("status").innerText = "Done!";
+  }
+
+  fileReader.readAsArrayBuffer(file);
+}
